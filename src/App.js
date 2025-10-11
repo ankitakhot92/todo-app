af5a6d860e753924dc7efc3d2bfa76e51aa5abb2
getdevopsready <getdevopsready@gmail.com>
2025-10-11 10:06:59 +0530

    Title updated

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    10WeeksofDevOps - TODO LIST
+                    10WeeksofCloudOps - TODO LIST
                 </Row>
   
                 <hr />
